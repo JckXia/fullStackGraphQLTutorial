5a0baa71705f2669b250e78467a873eddd401942
JckXia <31197636+JackXiaisagreatguy@users.noreply.github.com>
2019-12-27 15:40:56 -0500

    partial refaction complete

--- graphql/resolver/index.js
+++ graphql/resolver/index.js
@@ -1,9 +1,11 @@
 const bcrypt = require("bcryptjs");
 const Event = require("../../models/event");
 const User = require("../../models/user");
-
-const events = eventIds => {
-  return Event.find({ _id: { $in: eventIds } }).then(events => {
+const TEMP_USER_ID = "5e0660153f2c9239e44106d7";
+const events = async eventIds => {
+  console.log(`INFO--- Event IDS ${eventIds}`);
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
     return events.map(event => {
       return {
         ...event._doc,
@@ -11,41 +13,33 @@ const events = eventIds => {
         creator: user.bind(this, event.creator)
       };
     });
-  });
+  } catch (err) {
+    throw err;
+  }
 };
 
-const user = userId => {
-  return User.findById(userId)
-    .then(user => {
-      return {
-        ...user._doc,
-        _id: user.id,
-        createdEvents: events.bind(this, user._doc.createdEvents)
-      };
-    })
-    .catch(err => {
-      throw err;
-    });
+const user = async userId => {
+  try {
+    const userObject = await User.findById(userId);
+    return {
+      ...userObject._doc,
+      createdEvents: events.bind(this, userObject._doc.createdEvents)
+    };
+  } catch (err) {
+    throw err;
+  }
 };
 
 module.exports = {
-  events: () => {
-    return Event.find()
-      .populate("creator")
-      .then(events => {
-        return events.map(event => {
-          console.log(event);
-          return {
-            _id: event.id,
-            ...event._doc,
-            creator: user.bind(this, event._doc.creator)
-          };
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        throw err;
-      });
+  events: async () => {
+    const eventsObject = await Event.find();
+    return eventsObject.map(event => {
+      return {
+        _id: event.id,
+        ...event._doc,
+        creator: user.bind(this, event._doc.creator)
+      };
+    });
   },
   createEvent: args => {
     const event = new Event({
@@ -53,7 +47,7 @@ module.exports = {
       description: args.eventInput.description,
       price: +args.eventInput.price,
       date: new Date().toISOString(),
-      creator: "5e0660153f2c9239e44106d7"
+      creator: TEMP_USER_ID
     });
     let createdEvent;
     return event
@@ -63,7 +57,7 @@ module.exports = {
           ...result._doc,
           creator: user.bind(this, result._doc.creator)
         };
-        return User.findById("5e0660153f2c9239e44106d7");
+        return User.findById(TEMP_USER_ID);
       })
       .then(user => {
         if (!user) {
